c7878c12396b2167151b249dc3ddc8f4e44e96ef
icon <filaos974@hotmail.com>
2022-09-08 18:21:08 +0400

    sup message "" application prête a fonctionner hors ligne ? ""

--- src/components/ReloadPWA.vue
+++ src/components/ReloadPWA.vue
@@ -1,13 +1,12 @@
 <template>
-  <div v-if="offlineReady || needRefresh"
+  <div v-if="needRefresh"
        class="flex flex-wrap md:flex-nowrap bg-pink-900 text-white text-sm px-6 py-2 justify-between align-middle"
        role="alert">
     <div class="text-dark message mt-1">
-      <span v-if="offlineReady"> Application prête à fonctionner hors ligne </span>
-      <span v-else>Il y a des changement dans l'application, cliquer pour la mise à jour.</span>
+      <span>Il y a des changement dans l'application, cliquer pour la mise à jour.</span>
     </div>
     <div class="buttons flex align-middle mt-2 md:mt-0">
-      <button v-if="needRefresh" @click="updateCache()"
+      <button @click="updateCache()"
               class="w-full px-4 py-2 text-sm text-white leading-none transition-colors duration-150 bg-pink-900 border border-white rounded sm:w-auto active:bg-pink-600 hover:bg-pink-700 focus:outline-none focus:shadow-outline-purple mr-4">
         Mise à jour
       </button>
