dd7169245c45100e7f4f5ac687401d002bead82c
icon <filaos974@hotmail.com>
2022-09-09 00:20:37 +0400

    sup polyfill

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import '@/assets/css/bootstrap-5.0.2/bootstrap.min.css'
 import '@/assets/js/bootstrap-5.0.2/bootstrap.bundle.min.js'
 import '@/assets/css/style.css'
-import 'vite/modulepreload-polyfill'
 
 import App from './App.vue'
 
